ab330a5f523c3a978e9d744e1c89f0d1e90a0584
Lorenzo Buonanno <buonanno.lorenzo@gmail.com>
2023-05-04 13:00:55 +0200

    Add contacts and explainer

--- src/App.svelte
+++ src/App.svelte
@@ -24,6 +24,7 @@
           <tr>
             <td>
               <p
+                class="iframe-container"
                 on:mouseenter={() => {
                   prediction.active = true;
                 }}
@@ -50,22 +51,76 @@
                 day: "2-digit",
               })}
             </td>
-            <td style={prediction.last_activity_time > Date.now() ? "color:red" : ""}>
-              {new Date(prediction.last_activity_time).toLocaleDateString(undefined, {
-                year: "numeric",
-                month: "short",
-                day: "2-digit",
-              })}
+            <td
+              style={prediction.last_activity_time > Date.now()
+                ? "color:red"
+                : ""}
+            >
+              {new Date(prediction.last_activity_time).toLocaleDateString(
+                undefined,
+                {
+                  year: "numeric",
+                  month: "short",
+                  day: "2-digit",
+                }
+              )}
             </td>
           </tr>
         {/each}
       </tbody>
     </table>
   </div>
+  <div class="explainer">
+    <p>
+      If you want to receive a phone call when one of these markets implies a
+      probability higher than 30%, please text me on:
+    </p>
+    <ul class="contacts-list">
+      <li><a href="https://wa.me/00393461388774">Whatsapp</a></li>
+      <li><a href="https://signal.me/#p/+393461388774">Signal</a></li>
+      <li><a href="https://t.me/recursing">Telegram</a></li>
+      <li>
+        <a href="https://forum.effectivealtruism.org/users/lorenzo-buonanno">
+          EA Forum
+        </a>
+      </li>
+      <li>
+        <a href="https://www.lesswrong.com/users/lorenzo-buonanno">LessWrong</a>
+      </li>
+      <li>
+        <a href="https://www.facebook.com/profile.php?id=100073337767772">
+          Facebook
+        </a>
+      </li>
+      <li><a href="https://twitter.com/L___orenzo">Twitter</a></li>
+    </ul>
+    <p>
+      Write something like "I want you to call this number if a prediction
+      markets implies a 30% probability of nuclear war, I accept that there
+      might be false alarms". I will send you a text to verify that you own that
+      number, and will add it to a list of numbers that automatically get called
+      once one of the markets above implies a &gt;30% probability of nukes.
+    </p>
+    <p>
+      This is very heavily inspired from <a
+        href="https://hasrussialaunchednukesyet.com/"
+      >
+        hasrussialaunchednukesyet.com
+      </a>
+      (but even more extremely hacky).
+      <br />
+      See the script that checks the markets and makes the phone calls
+      <a
+        href="https://gist.github.com/Recursing/a9f88e0160435d97a35f6d9abe70c338"
+      >
+        here.
+      </a>
+    </p>
+  </div>
 </main>
 
 <style>
-  p {
+  .iframe-container {
     position: relative;
   }
   iframe {
@@ -75,7 +130,15 @@
     z-index: 10;
     pointer-events: none;
   }
+  .contacts-list {
+    list-style: none;
+    padding: 0;
+  }
   h1 {
     margin: 0;
+  }
+  .explainer {
+    margin: auto;
+    max-width: 100ch;
   }
 </style>
